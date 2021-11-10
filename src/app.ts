fd5230bb8c2d9b55af36b48473894e732606e64a
bryanbryan92 <breinah@unal.edu.co>
2021-11-10 18:04:04 -0500

    Preparacion de conexiones

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,15 @@ import express from 'express';
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+app.get('/ruta1/:id', (req, res) => {
+    console.log(req.headers);
+    console.log(req.params);
+    //console.log(req.query);
+    console.log(req.body);
     res.send('Prueba del servidor');
 });
 
